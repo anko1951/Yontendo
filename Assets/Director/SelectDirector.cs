1b571a4642faa58235d53956a01134a552bd39ef
Rena <yorisiroreihambx@gmail.com>
2024-12-04 10:51:54 +0900

    Fin:SelectDifficultScene:StepSelector

--- Assets/Director/SelectDirector.cs
+++ Assets/Director/SelectDirector.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class SelectDirector : MonoBehaviour
 {
+    [SerializeField]
+    UnityEngine.UI.Image[] images;
+    [SerializeField]
+    string[] stages;
+    [SerializeField]
+    string beforeSceneName;
+    [SerializeField]
+    float intarval;// 操作間隔
+
+
     private PlayerData playerData;
+    private int selectNow = 0;
+    private float timer;
 
     void Awake()
     {
@@ -27,6 +43,36 @@ public class SelectDirector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= intarval)
+        {
+            if(Input.GetAxis("Horizontal") != 0){
+            timer = 0;
+            images[selectNow].gameObject.SetActive(false);
+            float temp = Input.GetAxis("Horizontal");// A/D または ←/→
+            selectNow += (int)Mathf.Sign(temp);// 値を 1 または -1 に変換する
+            selectNow = Mathf.Clamp(selectNow,0,images.Length-1);
+            }
+            images[selectNow].gameObject.SetActive(true);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            ChangeScene();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Z)){
+            ReChangeScene();
+        }
     }
+
+    //選択したシーンへ
+    void ChangeScene(){
+        SceneManager.LoadScene(stages[selectNow]);
+    }
+
+    //一つ前のシーンへ
+    void ReChangeScene(){
+        SceneManager.LoadScene(beforeSceneName);
+    }
+
 }
